aaea46459c56658a2acb69a2d49b2a7637e01ed3
Vickie Dao <44853396+Vickie-D@users.noreply.github.com>
2024-04-07 00:46:27 -0500

    Update profilescreen.dart

--- lib/profilescreen.dart
+++ lib/profilescreen.dart
@@ -7,11 +7,58 @@ class ProfileScreen extends StatefulWidget {
   State<ProfileScreen> createState() => _ProfileScreenState();
 }
 
-class _ProfileScreenState extends State<ProfileScreen> {
+class _HomeScreenState extends State<ProfileScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DrawerExample(),
+    );
+  }
+}
+
+class DrawerExample extends StatefulWidget {
+  const DrawerExample({super.key});
+
+  @override
+  State<DrawerExample> createState() => _DrawerExampleState();
+}
+
+class _DrawerExampleState extends State<DrawerExample> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  void _openDrawer() {
+  _scaffoldKey.currentState!.openDrawer();
+  }
+
+  void _closeDrawer() {
+    Navigator.of(context).pop();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("Welcome to data page"),),
+      key: _scaffoldKey,
+      appBar: AppBar(title: const Text('Drawer Demo')),
+      body: Center(
+          child: Text("Welcome to data page"),
+
+      ),
+      drawer: Drawer(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text('This is the Drawer'),
+              ElevatedButton(
+                onPressed: _closeDrawer,
+                child: const Text('Close Drawer'),
+              ),
+            ],
+          ),
+        ),
+      ),
+      // Disable opening the drawer with a swipe gesture.
+      drawerEnableOpenDragGesture: false,
     );
   }
 }
